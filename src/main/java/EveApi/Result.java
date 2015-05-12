f34b2e7742c2e7742572d02bc2af5b442407c556
David Han <dhan@redhat.com>
2015-05-12 13:44:56 -0500

    Added working for EveApi access to pull character orders

---
+++ src/main/java/EveApi/Result.java
@@ -0,0 +1,21 @@
+package EveApi;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@SuppressWarnings("restriction")
+@XmlRootElement(name = "result")
+public class Result {
+
+    private RowSet rowset;
+
+    public RowSet getRowset() {
+        return rowset;
+    }
+
+    @XmlElement(name = "rowset")
+    public void setRowset(RowSet rowset) {
+        this.rowset = rowset;
+    }
+
+}
--- src/main/java/impl/Loader.java
+++ src/main/java/impl/Loader.java
@@ -1,13 +1,53 @@
 package impl;
 
-import trading.ShipManufacturing;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import EveApi.CharOrder;
 
 public class Loader {
 
     public static void main(String[] args) throws Exception {
-        String file = "C:\\Users\\David\\Dropbox\\Eve\\ShipManufacturing.xlsx";
+        String file = "C:\\Users\\David\\workspace\\EveMarketManager\\charOrder.txt";
+        String fullURL = "https://api.eveonline.com/char/MarketOrders.xml.aspx?keyID=4102574&vCode=bRm8i16U0kHCsI0mxHfsH28tgXQB65TRQ2eDijUkeOgAxW7jQPgrwhoNims1G49f";
+        EveCentral eve = new EveCentral(EveCentral.marketStatBase);
 
-        ShipManufacturing ship = new ShipManufacturing();
+        String fileString = readFile(file, StandardCharsets.UTF_8);
 
+        System.out.println(fileString);
+
+        CharOrder order = eve.unmarshal(fileString, CharOrder.class);
+
+        System.out.println(order.toString());
+    }
+
+    public static String queryEveCentralUrl(String fullURL) throws IOException {
+        URL obj = new URL(fullURL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return response.toString();
+    }
+
+    static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
     }
 }
